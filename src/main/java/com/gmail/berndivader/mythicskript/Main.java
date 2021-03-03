7560361e828f31417fca4fa851919439eb922fb4
BerndiVader <kasermandel@gmail.com>
2021-03-03 19:51:03 +0100

    minor changes in main

--- src/main/java/com/gmail/berndivader/mythicskript/Main.java
+++ src/main/java/com/gmail/berndivader/mythicskript/Main.java
@@ -9,6 +9,7 @@ import com.gmail.berndivader.mythicskript.conditions.Conditions;
 import com.gmail.berndivader.mythicskript.effects.Effects;
 import com.gmail.berndivader.mythicskript.events.Events;
 import com.gmail.berndivader.mythicskript.expressions.Expressions;
+import com.gmail.berndivader.mythicskript.functions.Functions;
 
 import ch.njol.skript.Skript;
 
@@ -20,8 +21,15 @@ public class Main extends JavaPlugin {
 	public void onEnable() {
 		plugin=this;
 		if(Bukkit.getPluginManager().getPlugin("MythicMobs")!=null&&Bukkit.getPluginManager().getPlugin("Skript")!=null){
+			
 			Skript.registerAddon(this);
-			register();
+			
+			CustomClasses.register();
+			Events.register();
+			Conditions.register();
+			Expressions.register();
+			Effects.register();
+			Functions.register();
 		}
 	}
 	
@@ -31,11 +39,4 @@ public class Main extends JavaPlugin {
 		Main.plugin = null;
 	}
 	
-	static void register() {
-		CustomClasses.register();
-		Events.register();
-		Conditions.register();
-		Expressions.register();
-		Effects.register();
-	}	
 }
