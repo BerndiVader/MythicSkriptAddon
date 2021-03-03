53046499b4ae4c6e59afc1674b5253ab19d22b0a
BerndiVader <kasermandel@gmail.com>
2021-03-03 19:48:54 +0100

    moved to functions/mechanics package

--- src/main/java/com/gmail/berndivader/mythicskript/functions/mechanics/SkriptfunctionMechanic.java
+++ src/main/java/com/gmail/berndivader/mythicskript/functions/mechanics/SkriptfunctionMechanic.java
@@ -1,4 +1,4 @@
-package com.gmail.berndivader.mythicskript.events;
+package com.gmail.berndivader.mythicskript.functions.mechanics;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -21,14 +21,14 @@ public class SkriptfunctionMechanic extends SkillMechanic implements INoTargetSk
 	Function<?>function;
 	Object[][]parameters;
 	int dataPos,locationPos,entityPos;
+	String name;
 	
 	public SkriptfunctionMechanic(String skill, MythicLineConfig mlc) {
 		super(skill, mlc);
 		
-		function=Functions.getFunction(mlc.getString("name",""));
-		if(function==null) {
-			Bukkit.getLogger().warning("Cant find function "+mlc.getString("name"));
-		} else {
+		name=mlc.getString("name","");
+		function=Functions.getFunction(name);
+		if(function!=null) {
 			parameters=new Object[function.getParameters().length][];			
 			dataPos=locationPos=entityPos=-1;
 			for(int i=0;i<function.getParameters().length;i++) {
@@ -45,41 +45,55 @@ public class SkriptfunctionMechanic extends SkillMechanic implements INoTargetSk
 					break;
 				}
 			}
+		} else {
+			Bukkit.getLogger().warning("Cant find function "+name);
 		}
 	}
 
 	@Override
 	public boolean cast(SkillMetadata meta) {
-		if(dataPos>-1) parameters[dataPos]=new SkillMetadata[] {meta};
-		if(locationPos>-1) parameters[locationPos]=new Location[0];
-		if(entityPos>-1) parameters[entityPos]=new Entity[0];
-		Object[]result=function.execute(parameters);
-		if(result!=null&&(result[0] instanceof Boolean)) {
-			return (boolean)result[0];
+		if(function!=null) {
+			if(dataPos>-1) parameters[dataPos]=new SkillMetadata[] {meta};
+			if(locationPos>-1) parameters[locationPos]=new Location[0];
+			if(entityPos>-1) parameters[entityPos]=new Entity[0];
+			Object[]result=function.execute(parameters);
+			if(result!=null&&(result[0] instanceof Boolean)) {
+				return (boolean)result[0];
+			}
+		} else {
+			Bukkit.getLogger().warning("Cant find function "+name);
 		}
 		return false;
 	}
 
 	@Override
 	public boolean castAtLocation(SkillMetadata meta, AbstractLocation aLocation) {
-		if(dataPos>-1) parameters[dataPos]=new SkillMetadata[] {meta};
-		if(locationPos>-1) parameters[locationPos]=new Location[] {BukkitAdapter.adapt(aLocation)};
-		if(entityPos>-1) parameters[entityPos]=new Entity[0];
-		Object[]result=function.execute(parameters);
-		if(result!=null&&(result[0] instanceof Boolean)) {
-			return (boolean)result[0];
+		if(function!=null) {
+			if(dataPos>-1) parameters[dataPos]=new SkillMetadata[] {meta};
+			if(locationPos>-1) parameters[locationPos]=new Location[] {BukkitAdapter.adapt(aLocation)};
+			if(entityPos>-1) parameters[entityPos]=new Entity[0];
+			Object[]result=function.execute(parameters);
+			if(result!=null&&(result[0] instanceof Boolean)) {
+				return (boolean)result[0];
+			}
+		} else {
+			Bukkit.getLogger().warning("Cant find function "+name);
 		}
 		return false;
 	}
 
 	@Override
 	public boolean castAtEntity(SkillMetadata meta, AbstractEntity aEntity) {
-		if(dataPos>-1) parameters[dataPos]=new SkillMetadata[] {meta};
-		if(locationPos>-1) parameters[locationPos]=new Location[0];
-		if(entityPos>-1) parameters[entityPos]=new Entity[] {aEntity.getBukkitEntity()};
-		Object[]result=function.execute(parameters);
-		if(result!=null&&(result[0] instanceof Boolean)) {
-			return (boolean)result[0];
+		if(function!=null) {
+			if(dataPos>-1) parameters[dataPos]=new SkillMetadata[] {meta};
+			if(locationPos>-1) parameters[locationPos]=new Location[0];
+			if(entityPos>-1) parameters[entityPos]=new Entity[] {aEntity.getBukkitEntity()};
+			Object[]result=function.execute(parameters);
+			if(result!=null&&(result[0] instanceof Boolean)) {
+				return (boolean)result[0];
+			}
+		} else {
+			Bukkit.getLogger().warning("Cant find function "+name);
 		}
 		return false;
 	}
