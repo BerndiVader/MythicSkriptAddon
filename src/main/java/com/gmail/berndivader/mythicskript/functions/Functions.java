203b4cb05888bcbb66443bc5a214238a2d8469f0
BerndiVader <kasermandel@gmail.com>
2021-03-05 15:20:19 +0100

    replaced skmechanic with skfunction alias for skriptfunction mechanic simplified skfunction for conditions, now uses only 2 alieas for all condition types added skriptfunction targeter for entities and locations.

--- src/main/java/com/gmail/berndivader/mythicskript/functions/Functions.java
+++ src/main/java/com/gmail/berndivader/mythicskript/functions/Functions.java
@@ -1,5 +1,6 @@
 package com.gmail.berndivader.mythicskript.functions;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -10,9 +11,15 @@ import com.gmail.berndivader.mythicskript.functions.conditions.CompareLocationsC
 import com.gmail.berndivader.mythicskript.functions.conditions.EntityCondition;
 import com.gmail.berndivader.mythicskript.functions.conditions.LocationCondition;
 import com.gmail.berndivader.mythicskript.functions.mechanics.SkriptfunctionMechanic;
+import com.gmail.berndivader.mythicskript.functions.targeters.EntityTargeter;
+import com.gmail.berndivader.mythicskript.functions.targeters.LocationTargeter;
 
+import ch.njol.skript.lang.function.Function;
+import ch.njol.skript.lang.function.Parameter;
 import io.lumine.xikage.mythicmobs.api.bukkit.events.MythicConditionLoadEvent;
 import io.lumine.xikage.mythicmobs.api.bukkit.events.MythicMechanicLoadEvent;
+import io.lumine.xikage.mythicmobs.api.bukkit.events.MythicTargeterLoadEvent;
+import io.lumine.xikage.mythicmobs.io.MythicLineConfig;
 
 public class Functions implements Listener {
 	
@@ -26,9 +33,10 @@ public class Functions implements Listener {
 	
 	@EventHandler
 	public void onMythicMobsCustomMechanicsLoad(MythicMechanicLoadEvent e) {
+		
 		switch(e.getMechanicName().toLowerCase()) {
 		case "skriptfunction":
-		case "skmechanic":
+		case "skfunction":
 			e.register(new SkriptfunctionMechanic(e.getContainer().getConfigLine(),e.getConfig()));
 			break;
 		}
@@ -36,28 +44,73 @@ public class Functions implements Listener {
 	
 	@EventHandler
 	public void onMythicMobsCustomConditionsLoad(MythicConditionLoadEvent e) {
+		
 		switch(e.getConditionName().toLowerCase()) {
-		case "skentitycondition":
-		case "skentity":
-			e.register(new EntityCondition(e.getConditionName(),e.getConfig()));
-			break;
-		case "sklocationcondition":
-		case "sklocation":
-			e.register(new LocationCondition(e.getConditionName(),e.getConfig()));
-			break;
-		case "skentitycomparecondition":
-		case "skentitycompare":
-			e.register(new CompareEntitiesCondition(e.getConditionName(),e.getConfig()));
-			break;
-		case "sklocationcomparecondition":
-		case "sklocationconpare":
-			e.register(new CompareLocationsCondition(e.getConditionName(),e.getConfig()));
-			break;
-		case "skentitylocationcomparecondition":
-		case "skentitylocationcompare":
-			e.register(new CompareEntityLocationCondition(e.getConditionName(),e.getConfig()));
+		case "skfunction":
+		case "skriptfunction":
+			MythicLineConfig mlc=e.getConfig();
+			String name=mlc.getString("name","");
+			Function<?>function=ch.njol.skript.lang.function.Functions.getFunction(name);
+			if(function!=null) {
+				if(function.getReturnType().getCodeName().equals("boolean")) {
+					Parameter<?>[]parameters=function.getParameters();
+					int size=parameters.length;
+					if(size==1) {
+						if (parameters[0].getType().getCodeName().equals("entity")) {
+							e.register(new EntityCondition(e.getConditionName(),e.getConfig(),function));
+						} else if(parameters[0].getType().getCodeName().equals("location")) {
+							e.register(new LocationCondition(e.getConditionName(),e.getConfig(),function));
+						} else {
+							Bukkit.getLogger().warning("No valid parameter found for "+name);
+						}
+					} else if(size==2) {
+						boolean para0=parameters[0].getType().getCodeName().equals("entity");
+						boolean para1=parameters[1].getType().getCodeName().equals("entity");
+						
+						if(para0&&para1) {
+							e.register(new CompareEntitiesCondition(e.getConditionName(),e.getConfig(),function));
+						} else if(!para0&&!para1) {
+							e.register(new CompareLocationsCondition(e.getConditionName(),e.getConfig(),function));
+						} else {
+							e.register(new CompareEntityLocationCondition(e.getConditionName(),e.getConfig(),function,para0));
+						}
+						
+					} else {
+						Bukkit.getLogger().warning("There was an error with the given paramters for "+name);
+					}
+				} else {
+					Bukkit.getLogger().warning("The return type for skriptcondition function "+name+" has to be boolean but is "+function.getReturnType().getCodeName());
+				}
+			} else {
+				Bukkit.getLogger().warning("Cant find function "+name);
+			}
 			break;
 		}
 	}
-
+	
+	@EventHandler
+	public void onMythicMobsCustomTargeterLoad(MythicTargeterLoadEvent e) {
+		
+		switch(e.getTargeterName().toLowerCase()) {
+		case "skfunction":
+		case "skriptfunction":
+			MythicLineConfig mlc=e.getConfig();
+			String name=mlc.getString("name","");
+			Function<?>function=ch.njol.skript.lang.function.Functions.getFunction(name);
+			if(function!=null) {
+				String returnType=function.getReturnType().getCodeName();
+				if(returnType.equals("location")) {
+					e.register(new LocationTargeter(mlc,function));
+				} else if(returnType.equals("entity")) {
+					e.register(new EntityTargeter(mlc, function));
+				} else {
+					Bukkit.getLogger().warning("Expected return type for skript targeter "+name+" has to be a entity or location list but is "+returnType);
+				}
+			} else {
+				Bukkit.getLogger().warning("Cant find function "+name);
+			}
+			break;
+		}
+		
+	}
 }
